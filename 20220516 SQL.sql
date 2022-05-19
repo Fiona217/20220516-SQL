9687fb64db61b7914bdfee61abb60100598dd1ce
Fiona217 <105613123+Fiona217@users.noreply.github.com>
2022-05-19 14:52:43 +1200

    Update 20220516 SQL.sql

--- 20220516 SQL.sql
+++ 20220516 SQL.sql
@@ -51,14 +51,10 @@ I think it should be setup base on the company's rules.
  
 --d).Display all the jobs available
 USE [Keys]
-SELECT *
-FROM [dbo].[Job]
-where [JobStatusId]=1
-and [OwnerId] IN (
-	SELECT [Id]
-	FROM [dbo].[Person]
-	WHERE [IsActive]=1
-)
+SELECT distinct a.[Id] AS [JobId], a.[PropertyId], a.[OwnerId], a.[JobDescription], c.[Status] AS [JobStatus]
+FROM [dbo].[Job] a
+INNER JOIN [dbo].[Person] b ON b.[Id]=a.[OwnerId] AND b.[IsActive]=1
+INNER JOIN [dbo].[JobStatus] c ON c.[Id]=a.[JobStatusId] AND c.[Id]=1
 
 --e).Display all property names, current tenants first and last names and rental payments per week/ fortnight/month for the properties in question a). 
 USE [Keys]
